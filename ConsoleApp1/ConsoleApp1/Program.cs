7e568dd05844b60067f1a11932855c1fa741419c
Brandon Ellingson <ellingsonbrandon33@gmail.com>
2025-06-08 11:53:28 -0500

    added 4 variables, very short story, and created story outside of main

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        CreateStory();
+    }
+
+    static void CreateStory()
+    {
+        Console.Write("Please enter your first name: ");
+        string userName = Console.ReadLine();
+
+        Console.Write("Please enter your favorite color: ");
+        string favColor = Console.ReadLine();
+
+        Console.Write("What is the animal that suits you the most?: ");
+        string animal = Console.ReadLine();
+
+        Console.Write("What is your favorite place on earth?: ");
+        string place = Console.ReadLine();
+        
+        Console.WriteLine($"{userName}'s {animal} is {favColor} and lives in {place}.");
+    }
+}
